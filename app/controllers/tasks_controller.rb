23fa145e44b9fd635fb8f619c2781a2410c4b4fc
teraitomoya <12a1070.sit.shonan@gmail.com>
2021-10-17 04:23:16 +0900

    新規追加画面動作確認

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,27 @@ class TasksController < ApplicationController
     @tasks = Task.all
     # taskテーブルから全てのレコードを取得しtaskのインスタンス変数に代入する
   end
+
+  def new
+    @task = Task.new
+# @taskはtasksではないけない。一度に作れるのは一つだけ
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+
+  end
+
+
+
+  private
+    def task_params
+      params[:task].permit(:title)
+    end
 end
